410f80c78daddc7daca2ed564e3c1b1f768a9244
betamemo <bandita.kul@gmail.com>
2024-04-23 11:33:17 +0700

    create route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'welcome']);
+
+Route::get('books', function () {
+    return view('books');
 });
