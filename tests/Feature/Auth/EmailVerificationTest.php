6da2bbf12cf96ea02e4618d45005cad7b851bb63
betamemo <bandita.kul@gmail.com>
2024-05-03 00:10:21 +0700

    edit auth

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 
 test('email verification screen can be rendered', function () {
-    $user = User::factory()->unverified()->create();
+    $user = User::factory()->create([
+        'email_verified_at' => null,
+    ]);
 
     $response = $this->actingAs($user)->get('/verify-email');
 
@@ -14,7 +16,9 @@ test('email verification screen can be rendered', function () {
 });
 
 test('email can be verified', function () {
-    $user = User::factory()->unverified()->create();
+    $user = User::factory()->create([
+        'email_verified_at' => null,
+    ]);
 
     Event::fake();
 
@@ -28,11 +32,13 @@ test('email can be verified', function () {
 
     Event::assertDispatched(Verified::class);
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(route('dashboard', absolute: false).'?verified=1');
+    $response->assertRedirect(route('dashboard', absolute: false) . '?verified=1');
 });
 
 test('email is not verified with invalid hash', function () {
-    $user = User::factory()->unverified()->create();
+    $user = User::factory()->create([
+        'email_verified_at' => null,
+    ]);
 
     $verificationUrl = URL::temporarySignedRoute(
         'verification.verify',
