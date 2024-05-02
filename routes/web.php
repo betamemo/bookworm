3d18acb127affaeede557811f54528dd88b67b0a
betamemo <bandita.kul@gmail.com>
2024-05-02 18:34:31 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,28 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Public Routes
-
+// Public routes
 Route::get('/', \App\Http\Controllers\WelcomeController::class);
 
-// User Routes
+Route::resource('authors', \App\Http\Controllers\AuthorController::class)->only('index', 'show');
+Route::resource('categories', \App\Http\Controllers\CategoryController::class)->only('index', 'show');
 
+Route::get('/books', [\App\Http\Controllers\BookController::class, 'index'])->name('books.index');
+Route::get('/books/{id}', [\App\Http\Controllers\BookController::class, 'show'])->name('books.show');
+
+Route::get('/search', [\App\Http\Controllers\SearchController::class, 'create'])->name('search.create');
+Route::post('/search', [\App\Http\Controllers\SearchController::class, 'store'])->name('search.store');
+
+// User routes
 require __DIR__ . '/auth.php';
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/dashboard', 'user/books')->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->prefix('user')->name('user.')->group(function () {
+    Route::resource('books', \App\Http\Controllers\User\BookController::class);
+    Route::resource('categories', \App\Http\Controllers\User\CategoryController::class)
+        ->middleware(\App\Http\Middleware\IsAdminMiddleware::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
