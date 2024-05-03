7707cec3f6f3163d3248a5b3ceffb2d633d2a473
betamemo <bandita.kul@gmail.com>
2024-05-03 14:57:43 +0700

    edit layout

--- resources/views/components/book-display-card.blade.php
+++ resources/views/components/book-display-card.blade.php
@@ -1,16 +1,15 @@
-<a href="{{route('books.show', ['id' => $book->id])}}" class="p-2 bg-white rounded hover:shadow-xl relative">
-    <img src="{{$book->getCardImageUrl()}}" class=" w-full">
+<a href="{{route('books.show', ['id' => $book->id])}}" class="rounded p-3 bg-white rounded hover:shadow-xl relative">
+    <img src="{{$book->getCardImageUrl()}}" class="w-full">
 
-    <div class="absolute flex flex-row gap-2 absolute top-4 right-4 text-xs">
-        @foreach($book->categories as $category)
-        <span class="bg-green-100 text-green-500 px-2 rounded-full">{{ $category->name }}</span>
-        @endforeach
-    </div>
+    <h3 class="font-semibold text-xl">{{$book->title}}</h3>
 
-    <h3 class="font-bold text-xl">{{$book->title}}</h3>
     <div>
-        <span class="text-green-500 font-bold">&gt</span>
-        {{$book->author}}
+        <span class="font-semibold text-gray-500">{{$book->author}}</span>
     </div>
-    <p class="line-clamp-3">{{ Str::limit($book->content,300)}}</p>
+
+    <!-- <p class="line-clamp-3">{{Str::limit($book->content,200)}}</p> -->
+
+    <!-- <div class="text-sm flex justify-end text-grey-200">
+        More details >
+    </div> -->
 </a>
