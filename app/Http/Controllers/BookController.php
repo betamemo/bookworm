5ea362386d336c74657c143dd50b91ee026fe7b9
betamemo <bandita.kul@gmail.com>
2024-05-03 14:55:51 +0700

    edit pagination

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Review;
 
 class BookController extends Controller
 {
     public function index()
     {
         $books = Book::with('author', 'media')
-            ->orderByDesc('created_at')->paginate(4);
+            ->orderByDesc('created_at')->paginate(8);
 
         return view('public.books.index', ['books' => $books]);
     }
@@ -20,7 +21,9 @@ class BookController extends Controller
 
         $related = $this->getRelatedBooks($book);
 
-        return view('public.books.show', ['book' => $book, 'related' => $related]);
+        $reviews = Review::where('book_id', $id)->get();
+
+        return view('public.books.show', ['book' => $book, 'related' => $related, 'reviews' => $reviews]);
     }
 
     private function getRelatedBooks(Book $book, $numberOfBooks = 6)
