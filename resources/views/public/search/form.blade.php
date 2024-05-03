e7b085acfaca9f9c947db3812890f927fbbde1e3
betamemo <bandita.kul@gmail.com>
2024-05-03 15:32:03 +0700

    edit form search

--- resources/views/public/search/form.blade.php
+++ resources/views/public/search/form.blade.php
@@ -1,10 +1,5 @@
 <x-site-layout title="Search and browse books">
 
-    <form action="/search" method="POST">
-        @csrf
-
-        <x-form-text name="search" label="Title" />
-        <button type="submit" class="p-1 text-sm bg-green-500 text-white rounded">Search</button>
-    </form>
+    <x-form-search />
 
 </x-site-layout>
--- resources/views/public/search/results.blade.php
+++ resources/views/public/search/results.blade.php
@@ -1,16 +1,13 @@
 <x-site-layout title="Search results for '{{$search_term}}'">
 
-    <div>Your search for '{{$search_term}}' resulted in {{$books->count()}} results:</div>
+<x-form-search />
 
-    @foreach($books as $book)
-    <ul>
-        <li>
-            <a href="{{route('books.show', ['id' => $book->id])}}" class="mt-2 hover:bg-green-50">
-                <h2 class="font-semibold">{{$book->title}}</h2>
-                <p>{{ Str::limit($book->content,100) }}</p>
-            </a>
-        </li>
-    </ul>
-    @endforeach
+    <div class="mb-4 mt-4">Your search for '{{ $search_term }}' resulted in {{ $books->count() }} results:</div>
+
+    <div class="grid grid-cols-4 gap-8">
+        @foreach($books as $book)
+        <x-book-display-card :book="$book" />
+        @endforeach
+    </div>
 
 </x-site-layout>
