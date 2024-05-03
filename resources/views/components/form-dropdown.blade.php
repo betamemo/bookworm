9c2c7fb031af136484e4ea4d87deaa7591dcb021
betamemo <bandita.kul@gmail.com>
2024-05-03 14:57:23 +0700

    add form dropdown

---
+++ resources/views/components/form-dropdown.blade.php
@@ -0,0 +1,6 @@
+<label for="status">Select a status:</label>
+<select name="status" id="status">
+    <option value="1">Want to Read</option>
+    <option value="2">Reading</option>
+    <option value="3">Finished</option>
+</select>
