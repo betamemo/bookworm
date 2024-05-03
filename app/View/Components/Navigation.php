bf38a45f7437f2b8c2d972b9088ebe2c0f85f583
betamemo <bandita.kul@gmail.com>
2024-05-03 14:52:08 +0700

    edit navigation

--- app/View/Components/Navigation.php
+++ app/View/Components/Navigation.php
@@ -10,23 +10,16 @@ class Navigation extends Component
 {
     public $menu = [];
 
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         $this->menu = [
             ['name' => 'Home', 'url' => '/'],
             ['name' => 'Books', 'url' => '/books'],
-            ['name' => 'Categories', 'url' => '/categories'],
             ['name' => 'Authors', 'url' => '/authors'],
             ['name' => 'Search', 'url' => '/search'],
         ];
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.navigation');
