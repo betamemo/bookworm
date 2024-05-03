a3e6fe67af87843389a882fbb3cbd1412a0b8054
betamemo <bandita.kul@gmail.com>
2024-05-03 15:45:54 +0700

    edit layout

--- resources/views/public/authors/index.blade.php
+++ resources/views/public/authors/index.blade.php
@@ -1,13 +1,13 @@
 <x-site-layout title="Authors">
 
-    <div class="flex justify-center mb-4">
+    <!-- <div class="flex justify-center mb-4">
         A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
-    </div>
+    </div> -->
 
-    <ul class="list-disc ml-5">
+    <ul class="list-disc">
         @foreach($authors as $author)
-        <li>
-            <a class="underline hover:bg-purple-200" href="/authors/{{$author->id}}">{{$author->name}}</a>
+        <li class="mb-4">
+            <a class="text-green-500 text-xl hover:underline" href="/authors/{{$author->id}}">{{ $author->name }}</a>
         </li>
         @endforeach
     </ul>
