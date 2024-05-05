0152f801ae52300892621eb10ce16f21693e63f9
betamemo <bandita.kul@gmail.com>
2024-05-05 17:13:53 +0700

    edit layout

--- resources/views/user/collections/index.blade.php
+++ resources/views/user/collections/index.blade.php
@@ -1,15 +1,16 @@
 <x-site-layout title="{{ auth()->user()->name }}'s collection">
 
-
-
     @foreach($collections as $collection)
-    <div class="p-1 mb-1">
-        <h2 class="font-bold text-xl mb-4 text-green-500">{{$collection->statuses->name}}</h2>
+    <div>
+        <h2 class="font-bold text-xl mb-4">{{$collection->statuses->name}}</h2>
 
         <div class="grid grid-cols-6 gap-6">
             <x-book-display-card :book="$collection->books" />
         </div>
 
+        <br />
+        <hr />
+        <br />
     </div>
     @endforeach
 
