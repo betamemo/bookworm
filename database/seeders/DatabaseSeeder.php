2dbc46713747b3ba78aa4ff152d24fddb0f03211
betamemo <bandita.kul@gmail.com>
2024-05-05 13:47:51 +0700

    seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
-use App\Models\BookStatus;
-use App\Models\Category;
+use App\Models\Collection;
+use App\Models\Genre;
 use App\Models\Review;
 use App\Models\Status;
 use App\Models\User;
@@ -15,32 +15,31 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Bandita',
+            'name' => 'Beta',
             'email' => 'beta@memo.com',
             'superadmin' => true,
             'password' => '$2y$12$bCueu1NNhB6S3YkUslUQy.4LUYBxNlieXD696R2dG/XT8LmuO51Y2', // 12345678
         ]);
 
-        User::factory(10)->create();
+        User::create([
+            'name' => 'Jeno',
+            'email' => 'jeno@dream.nct',
+            'superadmin' => false,
+            'password' => '$2y$12$bCueu1NNhB6S3YkUslUQy.4LUYBxNlieXD696R2dG/XT8LmuO51Y2', // 12345678
+        ]);
 
-        Category::factory(10)->create();
+        User::factory(5)->create();
 
         Status::create(['name' => 'Want to read']);
         Status::create(['name' => 'Reading']);
         Status::create(['name' => 'Finished']);
 
+        Genre::factory(20)->create();
+
         $books = Book::factory(20)->withImages()->create();
-
-        foreach ($books as $book) {
-
-            // Create array with 1 or 2 unique random numers between 1 and 5
-            $keyword_ids = collect([rand(1, 5), rand(1, 5)])->unique()->toArray();
-
-            $book->categories()->attach($keyword_ids);
-        }
 
         Review::factory(20)->create();
 
-        BookStatus::factory(20)->create();
+        Collection::factory(20)->create();
     }
 }
