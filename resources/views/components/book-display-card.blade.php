a2317d6b32e0b80a52a10ad2d1f6c9c71dcbd8cf
betamemo <bandita.kul@gmail.com>
2024-05-05 13:51:27 +0700

    edit layout

--- resources/views/components/book-display-card.blade.php
+++ resources/views/components/book-display-card.blade.php
@@ -1,13 +1,12 @@
-<a href="{{route('books.show', ['id' => $book->id])}}" class="p-4 bg-white rounded-xl border-solid border-2 border-gray-200 hover:shadow-xl">
-    <img src="{{$book->getCardImageUrl()}}" class=" w-full rounded-r-xl shadow-xl mb-2">
+<a href="{{ route('books.show', ['id' => $book->id]) }}" 
+class="p-4 bg-white rounded-xl hover:shadow-xl">
+    <img src="{{ $book->getCardImageUrl() }}" class=" w-full rounded-r-xl shadow-xl mb-2">
 
-    <h3 class="font-bold line-clamp-1">{{$book->name}}</h3>
+    <h3 class="font-bold line-clamp-1">{{ $book->name }}</h3>
 
-    <div class="text-sm">
-        {{$book->author}}
-    </div>
+    <div class="text-sm">{{ $book->author }}</div>
 
-    <p class="mt-4 text-xs line-clamp-3">{{Str::limit($book->content,300)}}</p>
+    <p class="mt-4 text-xs line-clamp-3">{{ Str::limit($book->content,300) }}</p>
 
-    <div class="mt-4 text-xs text-right text-green-500">Read more <span class="font-bold">&gt</span></div>
+    <div class="mt-4 text-xs text-right text-green-500">More detail <span class="font-bold">&gt</span></div>
 </a>
