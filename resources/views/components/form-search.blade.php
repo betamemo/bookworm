9ae419bd8ac7c075e67bb583416c033c967b5790
betamemo <bandita.kul@gmail.com>
2024-05-05 17:18:14 +0700

    edit layout

--- resources/views/components/form-search.blade.php
+++ resources/views/components/form-search.blade.php
@@ -1,9 +1,9 @@
 <form action="/search" method="POST">
     @csrf
 
-    <div class="w-full flex justify-center">
-        <input type="text" id="search" value="" class="p-2 rounded-full" name="search" placeholder="Title / Author / ISBN" />
+    <div class="w-full flex justify-center text-sm">
+        <input type="text" id="search" value="" class="p-2 w-full rounded-full" name="search" placeholder="Title / Author / ISBN" />
 
-        <button type="submit" class="ml-4 p-2 text-bold text-sm bg-green-500 text-white rounded-full hover:bg-green-600">Search</button>
+        <button type="submit" class="ml-4 p-2 font-bold bg-green-500 text-white rounded-full hover:bg-green-600">Search</button>
     </div>
 </form>
